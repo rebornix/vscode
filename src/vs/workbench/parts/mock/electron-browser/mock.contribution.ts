2f545001fb3ff1f867cb75a95991a5fa40389da9
isidor <inikolic@microsoft.com>
2016-04-15 11:57:19 +0200

    customDebugView: explain order

--- src/vs/workbench/parts/mock/electron-browser/mock.contribution.ts
+++ src/vs/workbench/parts/mock/electron-browser/mock.contribution.ts
@@ -7,5 +7,6 @@ import 'vs/css!../browser/media/mock.contribution';
 import { InformationView } from 'vs/workbench/parts/mock/browser/mockViews';
 import * as debug from 'vs/workbench/parts/debug/common/debug';
 
-// Register mock debug views
+// Register mock debug views.
+// Use order 25 to put the information view between the watch and call stack view (which have orders 20 and 30 respectively).
 debug.DebugViewRegistry.registerDebugView(InformationView, 25);
